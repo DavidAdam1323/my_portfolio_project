2ea6ee22cd784cdaa93f41fdedf253ddf5c8bcf9
DavidAdam1323 <davidadamgtr@gmail.com>
2024-05-05 02:20:33 +0100

    Update About Me section with in-depth overview of skills and collaborative mindset

--- src/app/components/AboutSection.jsx
+++ src/app/components/AboutSection.jsx
@@ -11,6 +11,28 @@ const AboutSection = () => {
           width={500}
           height={500}
         />
+        <div>
+          <h2 className="text-4xl font-bold mb-4">About Me</h2>
+          <p className="text-base lg:text-lg">
+            I am a passionate Full-Stack Software Developer dedicated to
+            creating engaging and user-friendly Web Applications. With hands-on
+            experience in JavaScript, React, Next.js, Node.js, PostgreSQL, HTML,
+            CSS, and Git, I possess a strong foundation in modern web
+            development technologies. Driven by a thirst for knowledge, I
+            actively seek opportunities to broaden my skill set and stay updated
+            with industry trends. I embrace challenges as learning opportunities
+            and continuously strive for excellence in my work. As a
+            collaborative team player, I thrive in environments where creativity
+            and innovation flourish. I am excited about the prospect of
+            collaborating with like-minded individuals to bring imaginative
+            ideas to life through exceptional applications.
+          </p>
+          <div className="flex flex-row mt-8">
+            <span>Skills</span>
+            <span>Experience</span>
+            <span>Education</span>
+          </div>
+        </div>
       </div>
     </section>
   );
