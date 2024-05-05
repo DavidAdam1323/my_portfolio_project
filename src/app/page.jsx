50b112b8284bd35bcaf063942c48fe335001306b
DavidAdam1323 <davidadamgtr@gmail.com>
2024-05-05 01:50:22 +0100

    Image added on AboutSection

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
+import AboutSection from "./components/AboutSection";
 
 export default function Home() {
   return (
@@ -8,6 +9,7 @@ export default function Home() {
       <Navbar />
       <div className="containe mt-24 max-auto px-12 py-4r">
         <HeroSection />
+        <AboutSection />
       </div>
     </main>
   );
