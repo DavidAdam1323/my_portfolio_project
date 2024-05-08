f310d025e3430888762ca8fe874a269aa6ea9921
DavidAdam1323 <davidadamgtr@gmail.com>
2024-05-08 23:36:05 +0100

    adding some styling inside ProjectCard component

--- src/app/components/ProjectCard.jsx
+++ src/app/components/ProjectCard.jsx
@@ -4,12 +4,13 @@ const ProjectCard = ({ imgUrl, title, description }) => {
   return (
     <div>
       <div
-        className="h-52 md:h-72"
+        className="h-52 md:h-72 rounded-t-xl relative"
         style={{ background: `url(${imgUrl})`, backgroundSize: "cover" }}
       ></div>
-      <div className="text-white">
-        <h5>{title}</h5>
-        <p>{description}</p>
+      <div className="overlay absolute"></div>
+      <div className="text-white rounded-b-xl bg-[#181818] py-6 px-4">
+        <h5 className="text-xl font-semibold mb-2">{title}</h5>
+        <p className="text-[#ADB7DB]">{description}</p>
       </div>
     </div>
   );
