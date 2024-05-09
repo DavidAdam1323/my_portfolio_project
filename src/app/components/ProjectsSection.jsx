6f49e4e2f4df56ffd010ada1e9db3306f69741c7
DavidAdam1323 <davidadamgtr@gmail.com>
2024-05-09 21:15:35 +0100

    ProjectTag.jsx component create and initialised

--- src/app/components/ProjectsSection.jsx
+++ src/app/components/ProjectsSection.jsx
@@ -50,7 +50,7 @@ const ProjectsSection = () => {
         <button className="rounded-full border-2 border-purple-500 px-6 py-3 text-xl cursor-pointer">
           All
         </button>
-        <button className="rounded-full border-2 border-slate-600 px-6 py-3 text-xl cursor-pointer">
+        <button className="rounded-full border-2 border-slate-600 hover:border-white px-6 py-3 text-xl cursor-pointer">
           Web
         </button>
       </div>
