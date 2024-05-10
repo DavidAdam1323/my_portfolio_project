1a0f212aad3a36bd17c244e21a948820b62cc524
DavidAdam1323 <davidadamgtr@gmail.com>
2024-05-11 00:08:55 +0100

    added som text inside EmailSection

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -5,7 +5,14 @@ const EmailSection = () => {
     <section className="grid grid-cols-2 my-12 md:my-12 py-24 gap-4">
       <div>
         <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
-        <p className="text-[#ADB7BE] mb-4 max-w-md "></p>
+        <p className="text-[#ADB7BE] mb-4 max-w-md ">
+          {" "}
+          I am currently exploring new opportunities and my inbox is always open
+          for any communication. Whether you have a query, wish to connect, or
+          simply want to say hello, please feel free to reach out. I am
+          dedicated to responding promptly and engaging with you in a meaningful
+          way.
+        </p>
       </div>
     </section>
   );
