ba985c982e9dbd9fa763cb9f057a06e1089e8fe8
DavidAdam1323 <davidadamgtr@gmail.com>
2024-05-11 14:12:35 +0100

    Footer completed

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Footer = () => {
-  return <footer>Footer</footer>;
+  return (
+    <footer className="footer border border-t-[#33353F] border-l-transparent border-r-transparent text-white">
+      <div className="container p-12 flex justify-between">
+        <span>LOGO</span>
+        <p className="text-slate-600">
+          &copy;David Adam Siva | All Rights Reserved.
+        </p>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
