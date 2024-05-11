64d11215472666bc3537aca0a032125ff43ecef4
DavidAdam1323 <davidadamgtr@gmail.com>
2024-05-11 11:16:37 +0100

    Fix unescaped single quote in EmailSection component JSX

--- src/app/components/EmailSection.jsx
+++ src/app/components/EmailSection.jsx
@@ -8,7 +8,9 @@ const EmailSection = () => {
   return (
     <section className="grid grid-cols-2 my-12 md:my-12 py-24 gap-4">
       <div>
-        <h5 className="text-xl font-bold text-white my-2">Let's Connect</h5>
+        <h5 className="text-xl font-bold text-white my-2">
+          Let&apos;s Connect
+        </h5>
         <p className="text-[#ADB7BE] mb-4 max-w-md ">
           {" "}
           I am currently exploring new opportunities and my inbox is always open
